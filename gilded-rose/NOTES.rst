2a9c0777046c5df8aa98bceb15ec111bcb895cdf
Chris Shenton <chris@v-studios.com>
2017-10-14 12:51:36 +0200

    test normals

---
+++ gilded-rose/NOTES.rst
@@ -0,0 +1,7 @@
+=======
+ NOTES
+=======
+
+Decrement sell_in *after* calculating value.
+
+Test1: Normal item quality--, sell_in=--
--- gilded-rose/test_gilded_rose.py
+++ gilded-rose/test_gilded_rose.py
@@ -4,8 +4,38 @@ from gilded_rose import GildedRose
 from item import Item
 
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    assert "fixme" == items[0].name
+def test_normal():
+    test_cases = [              # (before, after)
+        (Item("+5 Dexterity Vest", 2, 4),
+         Item("+5 Dexterity Vest", 1, 3)),
+        (Item('normal', 0, 0),
+         Item('normal', -1, 0)),
+        (Item('normal', 1, 0),
+         Item('normal', 0, 0)),
+        (Item('normal', 0, 1),
+         Item('normal', -1, 0)),
+        (Item('normal', 1, -1),
+         Item('normal', 0, -1)),  # VIOLATES SPEC, Q should be >= 0
+        (Item('normal', 1, 500),
+         Item('normal', 0, 499)),  # VIOLATES SPEC, Q should be <= 50
+        (Item("ñoqui", 1, 42),
+         Item("ñoqui", 0, 41)),
+    ]
+    items = [test_case[0] for test_case in test_cases]
+    guilded_rose = GildedRose(items)
+    guilded_rose.update_quality()
+    for i, item in enumerate(items):
+        assert items[i].name == test_cases[i][1].name
+        assert items[i].sell_in == test_cases[i][1].sell_in
+        assert items[i].quality == test_cases[i][1].quality
+
+
+
+# def test_sulfurus_never_changes():
+#     items = [Item("Sulfuras, Hand of Ragnaros","normal", 1, -1)]
+#     gilded_rose = GildedRose(items)
+#     gilded_rose.update_quality()
+#     assert items[0].name == 'normal'
+#     assert items[0].sell_in == 0
+#     assert items[0].quality == 0  # FAILS
+
